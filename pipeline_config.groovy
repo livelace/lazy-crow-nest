8dc76b7a42a5a656d5370d35448b26c9d1309b7c
Oleg 'livelace' Popov <o.popov@livelace.ru>
2021-09-09 14:17:24 +0300

    sonarqube

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -18,5 +18,6 @@ libraries {
        source = "dist/lazy_crow_nest-1.0.0-py3.8.egg"
        destination = "dists-internal/lazy-crow-nest/lazy-crow-nest-1.0.0-py3.8.egg"
     }
+    sonarqube
     python
 }
