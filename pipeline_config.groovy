46ceacf92a1f65eb0540765006f58f96b9208b01
Oleg 'livelace' Popov <o.popov@livelace.ru>
2021-09-10 11:12:45 +0300

    sonarqube

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,7 +2,7 @@ libraries {
     dependency_check
     dependency_track {
         project = "lazy-crow-nest"
-        version = "latest"
+        version = "master"
     }
     git {
         repo_url = "https://github.com/livelace/lazy-crow-nest.git"
