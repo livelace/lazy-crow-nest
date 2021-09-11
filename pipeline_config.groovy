f5b1b5ab91ca02e290940d8c1f0021eb16133f90
Oleg 'livelace' Popov <o.popov@livelace.ru>
2021-09-11 16:15:08 +0300

    alpine test

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -18,7 +18,6 @@ libraries {
     kaniko {
         destination = "data/lazy-crow-nest:latest"
     }
-    mattermost
     nexus {
        source = "dist/lazy_crow_nest-1.0.0-py3.8.egg"
        destination = "dists-internal/lazy-crow-nest/lazy-crow-nest-1.0.0-py3.8.egg"
