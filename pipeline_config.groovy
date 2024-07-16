1120770c29d1eaad1a2c273490e897314f0852bf
Oleg 'livelace' Popov <o.popov@livelace.ru>
2024-07-16 09:53:25 +0300

    ci refactor

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -13,9 +13,9 @@ libraries {
         repo_url = "${APP_REPO}"
         repo_branch = env.VERSION
     }
-    //harbor_replicate {
-    //    policy = "${APP_NAME}"
-    //}
+    harbor_replicate {
+        policy = "${APP_NAME}"
+    }
     k8s_build {
         image = "registry.livelace.ru/dev/lazy-crow-nest:latest"
 
